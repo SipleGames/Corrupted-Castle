432c3cee5eb9d90f160453a25c577e8591421378
Siple Games <80326424+SipleGames@users.noreply.github.com>
2021-06-07 14:51:04 +0200

    Add files via upload

--- web/game/js/cueva.js
+++ web/game/js/cueva.js
@@ -1,19 +1,36 @@
 //nombre para colocar portal->Portal
+var player;
 var tauro;
+var portal;
 var contadorArrow = 0;
 var heartText;
 var maxHearts = 3;
 var x;
 var y;
+var playermuerto = false;
+
 var KeyA;
+var KeyO;
 var KeyW;
 var KeyS;
 var KeySpace;
 var KeyD;
 var KeyV;
 var KeyP;
-var arrowList1;
-var enemyTauroList1;
+var KeyL, Key1, Key2, Key3, Key4, Key5;
+var arrowList;
+var enemyTauroList;
+var casilla1 = false;
+var casilla2 = false;
+var casilla3 = false;
+var casilla4 = false;
+var casilla5 = false;
+var inventory;
+var potionCasilla;
+var pocion;
+var potion;
+var apple;
+var Portal;
 
 
 class Cueva extends Phaser.Scene{
@@ -28,49 +45,53 @@ constructor()
 preload() {
 
   //Personaje
-    this.load.spritesheet('hero', 'game/assets/character/animMov.png', { frameWidth: 32, frameHeight: 32 });
-    this.load.spritesheet('roll', 'game/assets/character/Character_Roll.png',{ frameWidth: 32, frameHeight: 32 });
-    this.load.spritesheet('atack', 'game/assets/character/swordt.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('hero', 'assets/character/animMov.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('roll', 'assets/character/Character_Roll.png',{ frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('atack', 'assets/character/swordt.png', { frameWidth: 32, frameHeight: 32 });
 
   //Enemigo
-    this.load.spritesheet('enemyTauro', 'game/assets/enemies/tauro.png', {frameWidth: 50, frameHeight: 72});
+    this.load.spritesheet('enemyTauro', 'assets/enemies/tauro.png', {frameWidth: 50, frameHeight: 72});
 
   //Mapa
-    this.load.image("tiles", 'game/assets/tiles/[Base]BaseChip_pipo.png');
-    //this.load.image("sombras", 'assets/tiles/map_effect4.png');
-    this.load.image("sombras2", 'game/assets/tiles/[A]Wall-Up_Dungeon1_pipo.png');
-    this.load.tilemapTiledJSON("map", "game/assets/mapa/cueva.json");
+    this.load.image("tiles", 'assets/tiles/[Base]BaseChip_pipo16x16.png');
+    this.load.image("sombras", 'assets/tiles/[A]Wall-Up_Dungeon1_pipo.png');
+    this.load.tilemapTiledJSON("map", "assets/mapa/cueva.json");
+    this.load.image("portal", "assets/portal/portal.png");
 
   //Flechas
-    this.load.atlas('atlas', 'game/assets/arrow/arrow.png', 'assets/arrow/arrow.json');
+    this.load.atlas('atlas', 'assets/arrow/arrow.png', 'assets/arrow/arrow.json');
+
+  //Inventario
+    this.load.image('pocion', 'assets/inventario/pocion.png');
+    this.load.image('manzana', 'assets/inventario/manzana.png');
+    this.load.image('inventory', 'assets/inventario/inventario.png');
 
   //Corazones
-    this.load.image('heart', 'game/assets/health/heart.png');
-    this.load.image('heartempty', 'game/assets/health/border.png')
+    this.load.image('heart', 'assets/health/heart.png');
+    this.load.image('heartempty', 'assets/health/border.png')
 }
 
 create() {
-  
+  this.scene.add("Campo", new Campo); 
   //TILEMAP
   const map = this.make.tilemap({ key:"map" });
-  const tileset = map.addTilesetImage("[Base]BaseChip_pipo", "tiles");
-  const sombra = map.addTilesetImage("[A]Wall-Up_Dungeon1_pipo", "sombras2");
+  const tileset = map.addTilesetImage("[Base]BaseChip_pipo16x16", "tiles");
+  const sombra = map.addTilesetImage("[A]Wall-Up_Dungeon1_pipo", "sombras");
 
-  //const Techo = map.createStaticLayer("Techo", sombra, 0, 0);
-  const Piedras = map.createLayer("Piedras", tileset, 0, 0);
+  const Techo = map.createLayer("Top", tileset, 0, 0);
+  const Agujero = map.createLayer("Agujero", tileset, 0, 0);
   const Mundo = map.createLayer("World", tileset, 0, 0);
-  const Below = map.createLayer("Suelo", tileset, 0, 0);
-  
+  const Below = map.createLayer("Below", tileset, 0, 0);
   Below.setDepth(-1);
   Mundo.setCollisionByProperty({ collides: true });
   Techo.setCollisionByProperty({ collides: true });
-  Piedras.setCollisionByProperty({ collides: true });
+  Agujero.setCollisionByProperty({ collides: true });
   this.physics.world.setBounds(0, 0, 1600, 1600); 
   
 
   
   //SPAWNPOINT
-  const spawnPoint = map.findObject("Spawn Cueva", obj => obj.name === "Spawn Cueva");
+  const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Cueva");
 
   player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'hero');
 	player.setSize(10,14);
@@ -78,14 +99,27 @@ create() {
   player.speed = 175;
   player.speedRoll = 400;
 
+  //Portal
+   const Portal = map.findObject("Objects", obj => obj.name === "Portal");
+    var portalCampo = this.physics.add.sprite(Portal.x, Portal.y, "portal");
+    portalCampo.setOrigin(0.5);
+    portalCampo.setScale(0.2,0.2);
+    portalCampo.setDepth(-1);
+
 //Enemy
-  /*tauro = this.physics.add.group({
-    classType: 'enemyTauro'});*/
+  tauros = this.physics.add.group()
+  for (var i = 0; i < 9; i++)
+    {
+    /*Phaser.Math.Between(0, Mundo.width), Phaser.Math.Between(0, Mundo.height)*/
+      tauro = tauros.create(200, 200, 'Tauro2');
+      tauro.setScale(0.6);
+      tauro.velocidad = 60;  
+      tauro.patrolCircle = new Phaser.Geom.Circle(0, 0, 256);
+      tauro.direccion = -1;
+    }
 
   //Grupos
-  var arrowList1 = this.physics.add.group();
-  var enemyTauroList1 = this.physics.add.group();
-  
+  var arrowList = this.physics.add.group(); 
   player.setCollideWorldBounds(true);
   
   /*debugGraphics = this.add.graphics().setAlpha(0.7);
@@ -94,9 +128,23 @@ create() {
 		collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
 		faceColor: new Phaser.Display.Color(40, 39, 37, 255)
 	});*/
-  //Colision mundo player y enemigo
-  this.physics.add.collider(enemyTauroList1, Mundo);
-  this.physics.add.collider(Mundo, player)
+
+  //Inventario 
+  inventory = this.add.image(690, 30, 'inventory').setScrollFactor(0);
+  potion = this.physics.add.sprite(300, 200, 'pocion');
+  apple = this.physics.add.sprite(340, 150, 'manzana');
+
+  //Colision
+  this.physics.add.collider(tauro, Mundo, cambiar, null, this);
+  this.physics.add.collider(arrowList, Mundo);
+  this.physics.add.collider(Mundo, player);
+  this.physics.add.overlap(player, tauro, shake, null, this);
+  ;
+  this.physics.add.overlap(arrowList, tauro, enemyDie, null, this);
+
+  this.physics.add.overlap(player, potion, llevarinv, null, this);
+    this.physics.add.overlap(player, apple, llevarinv, null, this)
+  this.physics.add.overlap(portalCampo, player, changeCampo, null, this);
   
   
   
@@ -106,10 +154,10 @@ create() {
   this.cameras.main.startFollow(player);
 
 
-  this.physics.add.overlap(arrowList1, enemyTauroList1, destroyEnemy, null, this);
+  
 
   //CORAZONES
-  /*const hearts = this.add.group({
+  const hearts = this.add.group({
     classType: Phaser.GameObjects.Image
   })
   hearts.createMultiple({
@@ -120,10 +168,10 @@ create() {
       stepX: 18
     },
     quantity: 3
-  })*/
+  })
 
-  this.physics.add.collider(player, enemyTauroList1, shake, null, this);
-  heartText = this.add.text(16, 16, 'Hearts: '+ maxHearts, { fontSize: '16px', fill: '#ffc0cb' });
+  
+   heartText = this.add.text(16, 16, 'Hearts: '+ maxHearts, { fontSize: '16px', fill: '#ffc0cb' }).setScrollFactor(0);
 
 
   //MOVIMIENTO
@@ -134,52 +182,38 @@ create() {
     KeyO = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.O);
     KeyP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
     KeySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    Key1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
+    Key2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
+    Key3 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
+    Key4 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FOUR);
+    Key5 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FIVE);
+    KeyL = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
 
 //Animaciones movimiento
-    this.anims.create({
-      key: 'down',
-      frames: this.anims.generateFrameNumbers('hero', {start: 0, end: 3}),
-      frameRate: 10,
-      repeat: -1
-    });
-    this.anims.create({
-      key: 'up',
-      frames: this.anims.generateFrameNumbers('hero', {start: 12, end: 15}),
-      frameRate: 10,
-      repeat: -1
-    });
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('hero', {start: 4, end: 7}),
-      frameRate: 10,
-      repeat: -1
-    });
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('hero', {start: 8, end: 11}),
-      frameRate: 10,
-      repeat: -1
-    });
-    this.anims.create({
-      key: 'idleDown',
-      frames: [{ key: 'hero', frame: 0 }],
-      frameRate: 10
-    });
-    this.anims.create({
-      key: 'idleLeft',
-      frames: [{ key: 'hero', frame: 8 }],
-      frameRate: 10
-    });
-    this.anims.create({
-      key: 'idleUp',
-      frames: [{ key: 'hero', frame: 12 }],
-      frameRate: 10
-    });
-    this.anims.create({
-      key: 'idleRight',
-      frames: [{ key: 'hero', frame: 4 }],
-      frameRate: 10
-    });
+     this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('hero', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1
+      });
+      this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('hero', {start: 12, end: 15}),
+        frameRate: 10,
+        repeat: -1
+      });
+      this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('hero', {start: 4, end: 7}),
+        frameRate: 10,
+        repeat: -1
+      });
+      this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('hero', {start: 8, end: 11}),
+        frameRate: 10,
+        repeat: -1
+      });
 
 //Animaciones roll
     this.anims.create({
@@ -257,11 +291,24 @@ create() {
 
 }
 
-update() {
+update(time, delta) {
 
-  const taurospeed = 50;
+  for(var i = 0; i < 9; i++)
+  { 
+    tauro.body.setVelocityX(tauro.direccion * tauro.velocidad);
+ 
+    tauro.patrolCircle.x = player.x;
+    tauro.patrolCircle.y = player.y;
 
-  createEnemy();
+    if(tauro.patrolCircle.contains(player.x, player.y)){
+      if (player.x < tauro.x && tauro.body.setVelocityX >= 0) 
+        tauro.body.setVelocityX = -150;
+    }
+    else if (player.x > tauro.x && tauro.body.setVelocityX <= 0) {
+        tauro.body.setvelocityX = 150;
+    }
+
+  }
 
   //Codigo caminar
 
@@ -368,6 +415,31 @@ update() {
     }
     
   }
+  if (contadorArrow > 0)
+    {
+      contadorArrow = contadorArrow - 25;
+    }
+    player.body.velocity.normalize().scale(player.speed);
+
+  if(casilla1)
+  {
+    if (Phaser.Input.Keyboard.JustDown(Key1))
+    {
+      this.time.addEvent({delay: 6000, callback: acaboboost})
+      potionCasilla.destroy();
+      player.speed = 400; 
+      casilla1 = false;
+    }   
+  }
+
+  if(KeyL.isDown && casilla1 || casilla2 || casilla3 || casilla4 || casilla5)
+  {
+    potionCasilla.destroy();
+    potion = this.physics.add.sprite(player.x, player.y, 'pocion');
+    
+    this.physics.add.overlap(player, potion, llevarinv, null, this);
+    casilla1 = false;
+  }
 
 }
 }
@@ -463,19 +535,6 @@ function atackRight()
   player.anims.play('atackright', true);
 }
 
-function createEnemy()
-{
-  for (i = 0; i < 1; i++)
-  {
-    enemyTauroList1 = this.physics.add.sprite(200, 200, "enemyTauro");
-    enemyTauroList1.setOrigin(0.5, 0.5);
-    enemyTauroList1.setScale(0.55, 0.55);
-    enemyTauroList1.direccion = 0;
-    enemyTauroList1.setSize(30,40);
-    enemyTauroList1.speed = 100;
-  }
-}
-
 function atackUp()
 {
   player.anims.play('atackup', true);
@@ -485,35 +544,136 @@ function destroyEnemy(a, e)
 {
   a.disableBody(true, true);
   e.disableBody(true, true);
-  arrowList1.remove(a);
-  enemyTauroList1.remove(e);
+  arrowList.remove(a);
+  tauro.remove(e);
 }
 
-function shake(p, tl){
-  /*p.disableBody(false, false);
-  tl.disableBody(true, true);
-  enemyTauroList.remove(tl);
+function shake(){
+  const hearts = this.add.group({
+  classType: Phaser.GameObjects.Image
+  })
+  hearts.createMultiple({
+    key: 'heart',
+    setXY: {
+      x: 20,
+      y: 10,
+      stepX: 18
+    },
+    quantity: 2
+  })
+    
   player.hearts -=1;
-  heartText.text = 'Hearts: ' + player.hearts;*/
+  heartText.text = 'Hearts: ' + player.hearts;
   if(player.hearts <= 0)
   {
-    killPlayer();
+    playerdie();
   }
 
 }
 
-function killPlayer()
+function playerdie()
 {
-  player.dead = true;
-  killSprite(player, restartGame);
+   player.disableBody(true, true);
+    playermuerto = true;
 }
 
-function killSprite()
+function enemyDie()
 {
-  player.destroy();
+  tauro.disableBody(true, true);
+
 }
 
-function restartGame()
+function arrowCreatorLeft()
 {
-  game.state.start(game.state.current);
+  for (i = 0; i < 1; i++)
+  {
+    arrow = arrowList.create(player.x, player.y, 'atlas', 'left');
+    arrow.setScale(0.10, 0.10);
+    arrow.setOrigin(0.5, 0.5);
+    arrow.speed = 250;
+    arrow.setDepth(-1);
+    arrow.setSize(10,14);
+
+    arrow.setVelocityX(-arrow.speed);
+  }
+}
+function arrowCreatorUp()
+{
+  for (i = 0; i < 1; i++)
+  {
+    arrow = arrowList.create(player.x, player.y, 'atlas', 'up');
+    arrow.setScale(0.10, 0.10);
+    arrow.setOrigin(0.5, 0.5);
+    arrow.speed = 250;
+    arrow.setDepth(-1);
+    arrow.setSize(10,14);
+
+    arrow.setVelocityY(-arrow.speed);
+  }
+}
+
+function arrowCreatorRight()
+{
+  for (i = 0; i < 1; i++)
+  {
+    arrow = arrowList.create(player.x, player.y, 'atlas', 'right');
+    arrow.setScale(0.10, 0.10);
+    arrow.setOrigin(0.5, 0.5);
+    arrow.speed = 250;
+    arrow.setDepth(-1);
+    arrow.setSize(10,14);
+
+    arrow.setVelocityX(arrow.speed);
+  }
+}
+
+function arrowCreatorDown()
+{
+  for (i = 0; i < 1; i++)
+  {
+    arrow = arrowList.create(player.x, player.y, 'atlas', 'down');
+    arrow.setScale(0.10, 0.10);
+    arrow.setOrigin(0.5, 0.5);
+    arrow.speed = 250;
+    arrow.setDepth(-1);
+    arrow.setSize(10,14);
+
+   arrow.setVelocityY(arrow.speed);
+  }
+}
+
+
+function caeragujero()
+{
+  playerdie();
+}
+
+function cambiar()
+{
+  if(tauro.direccion = 1){tauro.direccion = 1}
+  else{tauro.direccion = -1}
+  
+}
+
+function llevarinv()
+{
+	potion.destroy();
+	potionCasilla = this.add.image(618, 35, 'pocion').setScrollFactor(0);
+	potionCasilla.setScale(2);
+  casilla1 = true;
+}
+
+function acaboboost()
+{
+  player.speed = 175;
+}
+
+function changeCampo()
+{
+  this.scene.start("Campo");
+  this.scene.remove("Castillo");
+  this.scene.remove("Cueva");
+  
+  
+
 }
